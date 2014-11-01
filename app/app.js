f04077550ccc11824289ac7a61d35adecb07b89a
Tina Zheng <tinazheng@berkeley.edu>
2014-11-01 16:28:56 -0700

    started view files

--- app/app.js
+++ app/app.js
@@ -4,9 +4,14 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
+  'myApp.view3',
   'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+$.get(
+	"https://api.venmo.com/v1/me?access_token=4e4sw1111111111t8an8dektggtcbb45"
+);
